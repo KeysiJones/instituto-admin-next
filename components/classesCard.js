caa155dd4e913cbfaf8c9223c2f861869398d5d4
keysijones <keysijones01@gmail.com>
2022-01-19 15:51:18 -0300

    feat: improves login ui

--- components/classesCard.js
+++ components/classesCard.js
@@ -41,9 +41,9 @@ const ClassesCard = ({ urls, aulas, diaSemana, setRefresh }) => {
       <p className="my-12 text-4xl text-blue-400">{dia[diaSemana]}</p>
       <button
         onClick={() => handleInsertion()}
-        className="bg-blue-500 p-1 text-white rounded-t-xl outline-none font-bold"
+        className="bg-blue-500 p-2 text-white rounded-t-xl outline-none font-bold"
       >
-        Cadastrar nova aula de {dia[diaSemana]}
+        Cadastrar nova aula de {dia[diaSemana].toLowerCase()}
       </button>
       <div
         border
--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,8 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { api } from "../utils/api";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { Loader } from "../components/loader";
 
 const LoginValidationSchema = Yup.object().shape({
   username: Yup.string()
@@ -16,68 +18,76 @@ const LoginValidationSchema = Yup.object().shape({
 
 export default function Login() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   return (
     <div>
-      <Formik
-        validationSchema={LoginValidationSchema}
-        validateOnBlur={false}
-        initialValues={{ username: "", password: "" }}
-        onSubmit={({ username, password }) => {
-          api
-            .post("login", { json: { username, password } })
-            .json()
-            .then((resposta) => {
-              if (resposta.status === 200 && resposta.token) {
-                localStorage.setItem("authToken", resposta.token);
-                alert("Login realizado com sucesso");
-                router.replace("/");
-              }
-            })
-            .catch((error) =>
-              alert("Usuário e/ou senha inválidos, tente novamente")
-            );
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div className="flex flex-col w-screen h-screen justify-center items-center">
-              <div>
-                <label className="mr-2">Usuário</label>
-                <Field
-                  type="text"
-                  name="username"
-                  className="border-2 px-2 rounded-md"
-                />
-                <ErrorMessage
-                  className="text-red-400"
-                  name="username"
-                  component="div"
-                />
-              </div>
-              <div>
-                <label className="mr-2">Senha</label>
-                <Field
-                  type="password"
-                  name="password"
-                  className="border-2 px-2 rounded-md"
-                />
-                <ErrorMessage
-                  className="text-red-400"
-                  name="password"
-                  component="div"
-                />
-              </div>
-              <button
-                className="px-2 mt-2 rounded-md bg-green-400 text-white"
-                type="submit"
-              >
-                Enviar
-              </button>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Formik
+          validationSchema={LoginValidationSchema}
+          validateOnBlur={false}
+          initialValues={{ username: "", password: "" }}
+          onSubmit={({ username, password }) => {
+            setLoading(true);
+            api
+              .post("login", { json: { username, password } })
+              .json()
+              .then((resposta) => {
+                setLoading(false);
+                if (resposta.status === 200 && resposta.token) {
+                  localStorage.setItem("authToken", resposta.token);
+                  alert("Login realizado com sucesso");
+                  router.replace("/");
+                }
+              })
+              .catch((error) => {
+                setLoading(false);
+                alert("Usuário e/ou senha inválidos, tente novamente");
+              });
+          }}
+        >
+          {({ isSubmitting }) => (
+            <div className="flex flex-col h-screen justify-center items-center">
+              <Form>
+                <div>
+                  <Field
+                    type="text"
+                    name="username"
+                    placeholder="Usuário"
+                    className="border-2 px-2 py-2 rounded-md my-2 outline-none"
+                  />
+                  <ErrorMessage
+                    className="text-red-400"
+                    name="username"
+                    component="div"
+                  />
+                </div>
+                <div>
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Senha"
+                    className="border-2 px-2 py-2 rounded-md outline-none"
+                  />
+                  <ErrorMessage
+                    className="text-red-400"
+                    name="password"
+                    component="div"
+                  />
+                </div>
+                <button
+                  className="py-2 last:mt-2 w-full rounded-md bg-blue-400 text-white"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </Form>
             </div>
-          </Form>
-        )}
-      </Formik>
+          )}
+        </Formik>
+      )}
     </div>
   );
 }
